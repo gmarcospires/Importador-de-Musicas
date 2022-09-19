d3f68eaa8b635586d904a97dacb11cc22889f9a9
Marcos <40155106+gmarcospires@users.noreply.github.com>
2022-09-19 10:56:46 -0300

    Construindo Página inicial

---
+++ components/Button/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Button = (props) =>{
+    return (
+        <Link href={props.href}>
+        <button type={props.type ? props.type : 'button'} className={props.className} onClick={props.onClick}>{props.children}</button>
+        </Link>
+    )
+}
+
+export default Button;
