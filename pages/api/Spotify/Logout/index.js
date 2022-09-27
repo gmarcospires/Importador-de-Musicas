c80bd39d6035e7648aa134502afae27894d4872c
Marcos <40155106+gmarcospires@users.noreply.github.com>
2022-09-27 20:34:35 -0300

    Fixed: cookie incorreto em /api/spotify/logout

--- pages/api/Spotify/Logout/index.js
+++ pages/api/Spotify/Logout/index.js
@@ -2,8 +2,8 @@ import { deleteCookie } from 'cookies-next';
 import { stateKey } from '../../../../js/spotify_auth.js';
 
 export default function handler(req, res) {
-  deleteCookie('access_token', { req, res });
-  deleteCookie('refresh_token', { req, res });
+  deleteCookie('access_token_spotify', { req, res });
+  deleteCookie('refresh_token_spotify', { req, res });
   deleteCookie(stateKey, { req, res });
   res.status(200).json({ message: 'Logged out' });
 }
