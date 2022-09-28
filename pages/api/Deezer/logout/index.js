8fb2950baa685830e39ff8de955d8056b23f556d
Marcos <40155106+gmarcospires@users.noreply.github.com>
2022-09-27 21:19:25 -0300

    Tentativa de correcao de bug das pastas

--- pages/api/Deezer/logout/index.js
+++
@@ -1,9 +0,0 @@
-import { stateKey } from '../../../../js/deezer_auth.js';
-import { deleteCookie } from 'cookies-next';
-
-export default function handler(req, res) {
-  deleteCookie('access_token_deezer', { req, res });
-  deleteCookie('refresh_token_deezer', { req, res });
-  deleteCookie(stateKey, { req, res });
-  res.status(200).json({ message: 'Logged out' });
-}
