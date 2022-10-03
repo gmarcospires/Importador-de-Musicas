48c8cbeb9cd28525989b693acb56eb6c15edb38e
Marcos <40155106+gmarcospires@users.noreply.github.com>
2022-10-03 19:13:34 -0300

    Ajustes nas variáveis de desenvolvimento

--- js/spotify_auth.js
+++ js/spotify_auth.js
@@ -1,7 +1,7 @@
 // Secret environment variables that must be set
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
+const client_id = process.env.CLIENT_ID_SPOTIFY;
+const client_secret = process.env.CLIENT_SECRET_SPOTIFY;
 const redirect_uri = process.env.REDIRECT_URI_SPOTIFY;
-const stateKey = "spotify_auth_state";
+const stateKey = 'spotify_auth_state';
 
 module.exports = { client_id, client_secret, redirect_uri, stateKey };
